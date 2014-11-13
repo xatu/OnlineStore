6173053a73bf4b9bd4b67e8f45471546f0cc7417
xatu <rafaelf.mezquita@gmail.com>
2014-11-13 01:38:07 -0400

    Se agregro bootstrap, un template, organizacion de los archivos, estilo bootstrap primefaces y eliminacion del ejemplo imagen.xhtml

---
+++ src/java/beans/ImagenBean.java
@@ -0,0 +1,56 @@
+package beans;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.FileUploadEvent;
+
+@ManagedBean
+@SessionScoped
+public class ImagenBean implements Serializable {
+
+    private String imagen;
+
+    public ImagenBean() {
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+    
+    public void uploadFile(FileUploadEvent event){
+        try{
+            String path = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/resources/img");
+            String archivo = path + File.separator + event.getFile().getFileName();
+            
+            FileOutputStream fileOutputStream = new FileOutputStream(archivo);
+            byte[] buffer = new byte[6124];
+            int bulk;
+            InputStream inputStream = event.getFile().getInputstream();
+            while (true) {
+                bulk = inputStream.read(buffer);
+                if (bulk < 0) {
+                    break;
+                }
+                fileOutputStream.write(buffer,0,bulk);
+                fileOutputStream.flush();
+            } 
+            fileOutputStream.close();
+            inputStream.close();
+            
+            this.setImagen(event.getFile().getFileName());
+            
+        }catch (IOException e){
+            e.printStackTrace();
+        }        
+    }
+}
---
+++ src/java/beans/ProductoManagedBean.java
@@ -0,0 +1,47 @@
+package beans;
+
+import java.io.Serializable;
+import model.Producto;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "productoManagedBean")
+@SessionScoped
+public class ProductoManagedBean implements Serializable {
+
+    private Producto producto;
+    private List<Producto> productos;
+    
+    public ProductoManagedBean() {
+        
+    }
+
+    @PostConstruct
+    public void init() {
+        producto = new Producto();
+        productos = new ArrayList<>();             
+    }
+    
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+
+    public void addProductos() {        
+        this.productos.add(this.producto);
+    }
+}
